7b34b7ef2ec335d9417a82f70959740f5e470878
Yaroslav Berkut <yaroslav.berkut@gmail.com>
2019-05-06 19:19:22 +0300

    comment failed test assertion

--- src/App.test.js
+++ src/App.test.js
@@ -6,5 +6,5 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-  expect(true).equal(false);
+  // expect(true).equal(false);
 });
